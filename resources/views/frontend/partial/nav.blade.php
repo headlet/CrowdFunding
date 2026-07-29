82dc60c9bb9133dca5c45d8b4e203942d826f04a
headlet <professionalskill6@gmail.com>
2026-07-29 17:36:38 +0545

    fixed mobile menu in nav

--- resources/views/frontend/partial/nav.blade.php
+++ resources/views/frontend/partial/nav.blade.php
@@ -10,10 +10,12 @@
     Mobile Menu
   ============================== -->
 <div class="th-menu-wrapper">
-    <div class="th-menu-area text-center">
+    <div class="text-center th-menu-area">
         <button class="th-menu-toggle"><i class="fal fa-times"></i></button>
         <div class="mobile-logo">
-            <a href="{{ route('index') }}"><img src="{{ asset(!empty($generalSetting->header_logo) ? 'storage/'. $generalSetting->header_logo : 'img/logo.png') }}" alt="Donat"></a>
+            <a href="{{ route('index') }}"><img
+                    src="{{ asset(!empty($generalSetting->header_logo) ? 'storage/' . $generalSetting->header_logo : 'img/logo.png') }}"
+                    alt="Donat"></a>
         </div>
         <div class="th-mobile-menu">
             <ul>
@@ -23,7 +25,7 @@
                 </li>
 
                 <li><a href="{{ route('about') }}">About Us</a></li>
-                <li >
+                <li>
                     <a href="{{ route('campaign') }}">Campaigns</a>
                 </li>
                 <li class="menu-item-has-children">
@@ -44,7 +46,22 @@
                     <a href="{{ route('contact') }}">Contact Us</a>
                 </li>
 
+
+                <div>
+                    @guest
+                        <a href="{{ route('login') }}" class="th-btn style3 w-100">
+                            <i class="fas fa-sign-in-alt me-2"></i> Login
+                        </a>
+                    @else
+                        <a href="{{ route('admin.dashboard') }}" class="th-btn style3 w-100">
+                            <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+                        </a>
+                    @endguest
+                </div>
+
             </ul>
+
+
         </div>
     </div>
 </div>
@@ -58,7 +75,9 @@
         <div class="container">
             <div class="menu-area">
                 <div class="header-logo">
-                    <a href="{{ route('index') }}"><img src="{{ asset(!empty($generalSetting->header_logo) ? 'storage/'. $generalSetting->header_logo : 'img/logo.png') }}" alt="Donat"></a>
+                    <a href="{{ route('index') }}"><img
+                            src="{{ asset(!empty($generalSetting->header_logo) ? 'storage/' . $generalSetting->header_logo : 'img/logo.png') }}"
+                            alt="Donat"></a>
                 </div>
                 <div class="menu-area-wrap">
                     <nav class="main-menu d-none d-lg-block">
@@ -119,8 +138,8 @@
 
 <div class="color-scheme-wrap active">
     <button class="switchIcon"><i class="fa-solid fa-palette"></i></button>
-    <h3 class="color-scheme-wrap-title text-center">Color Switcher</h3>
-    <h4 class="color-scheme-wrap-subtitle text-center">Theme Color</h4>
+    <h3 class="text-center color-scheme-wrap-title">Color Switcher</h3>
+    <h4 class="text-center color-scheme-wrap-subtitle">Theme Color</h4>
     <div class="color-switch-btns">
         <button data-color="#1A685B"><i class="fa-solid fa-droplet"></i></button>
         <button data-color="#f34e3a"><i class="fa-solid fa-droplet"></i></button>
@@ -128,7 +147,7 @@
         <button data-color="#3843C1"><i class="fa-solid fa-droplet"></i></button>
         <button data-color="#FF7E02"><i class="fa-solid fa-droplet"></i></button>
     </div>
-    <h4 class="color-scheme-wrap-subtitle mt-20 text-center">Secondary Color</h4>
+    <h4 class="mt-20 text-center color-scheme-wrap-subtitle">Secondary Color</h4>
     <div class="secondary-color-switch-btns">
         <button data-secondary-color="#FFAC00"><i class="fa-solid fa-droplet"></i></button>
         <button data-secondary-color="#F41E1E"><i class="fa-solid fa-droplet"></i></button>
